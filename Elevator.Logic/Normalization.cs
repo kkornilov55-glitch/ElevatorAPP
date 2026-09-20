9d9a11e175cb9d3979b5e84f4d7edc39e1e39b73
Niki <zxcvbrtm12345@gmail.com>
2026-09-20 22:20:47 +0300

    -Создание класса Normalization -Добавлен основной метод Normalize -В Normalize добавленны Pre-условия

---
+++ Elevator.Logic/Normalization.cs
@@ -0,0 +1,33 @@
+﻿namespace Elevator.Logic
+{
+    public class Normalization
+    {
+        // Основной метод
+        public string Normalize(string text)
+        {
+            // Pre-условия
+            if (text == null)
+            {
+                throw new ArgumentNullException(nameof(text), "Pre-условие нарушено: текст не может быть null");
+            }
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return "";
+            }
+
+            string result = text;
+
+            ///Методы для обработки теста:        
+            /// 1. Приведение к нижнему регистру
+            /// 2. Удаление пунктуации и спецсимволов
+            /// 3. Замена переносов строк и табуляций на пробелы
+            /// 4. Удаление пробелов в начале и конце
+            /// ...
+            // Post-условия
+
+
+            return result;
+        }
+    }
+}
