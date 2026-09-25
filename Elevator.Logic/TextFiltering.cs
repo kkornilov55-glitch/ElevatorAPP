be5cc4486018f6dee88159bcd465e07a79934271
Kirill <kkornilov55@gmail.com>
2026-09-25 17:30:03 +0300

    refactor[TextFiltering]: удалены лишние импорты

--- Elevator.Logic/TextFiltering.cs
+++ Elevator.Logic/TextFiltering.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Elevator.Logic
+﻿namespace Elevator.Logic
 {
     /// <summary>
     /// Класс для фильтрации текста по ключевому слову.
